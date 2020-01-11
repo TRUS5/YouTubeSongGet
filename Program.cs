befd520ae592c220af46778a2ee1f2fc63107665
TRUS5 <trus888@gmail.com>
2020-01-11 03:31:38 +0300

    cleanup

--- Program.cs
+++ Program.cs
@@ -1,47 +1,43 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Threading;
-using System.Windows.Automation;
-using System.Text;
-using System.Windows.Forms;
 using System.Net;
 using System.Net.Mail;
 using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Forms;
 
 namespace MilashkaChannel
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
-
-            DialogResult dialogResult2 = MessageBox.Show("Вы хотите отправить TRUS5 свой ip-адрес?", "", MessageBoxButtons.YesNo);
+            var dialogResult2 =
+                MessageBox.Show("Вы хотите отправить TRUS5 свой ip-адрес?", "", MessageBoxButtons.YesNo);
             if (dialogResult2 == DialogResult.Yes)
             {
                 var temptext = "";
 
 
-                foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+                foreach (var netInterface in NetworkInterface.GetAllNetworkInterfaces())
                 {
                     temptext += "Name: " + netInterface.Name + "\n";
                     temptext += "Description: " + netInterface.Description + "\n";
                     temptext += "Addresses: \n";
-                    IPInterfaceProperties ipProps = netInterface.GetIPProperties();
-                    foreach (UnicastIPAddressInformation addr in ipProps.UnicastAddresses)
-                    {
-                        temptext += addr.Address.ToString() + "\n";
-                    }
+                    var ipProps = netInterface.GetIPProperties();
+                    foreach (var addr in ipProps.UnicastAddresses) temptext += addr.Address + "\n";
                 }
 
-                MailMessage mail = new MailMessage();
+                var mail = new MailMessage();
                 mail.From = new MailAddress("ipstealervor@yandex.ru");
                 mail.To.Add(new MailAddress("trus888@gmail.com"));
                 mail.Subject = "KEK";
                 mail.Body = temptext;
 
-                SmtpClient client = new SmtpClient();
+                var client = new SmtpClient();
                 client.Host = "smtp.yandex.ru";
                 client.Port = 587;
                 client.EnableSsl = true;
@@ -50,75 +46,55 @@ namespace MilashkaChannel
             }
             else if (dialogResult2 == DialogResult.No)
             {
-              
             }
 
 
             if (!File.Exists("Song.txt"))
-            {
-                using (FileStream fs = File.Create("Song.txt"))
+                using (var fs = File.Create("Song.txt"))
                 {
-                    Byte[] info = new UTF8Encoding(true).GetBytes("Тут будет песня Коляна.");
+                    var info = new UTF8Encoding(true).GetBytes("Тут будет песня Коляна.");
                     fs.Write(info, 0, info.Length);
                 }
-            }
 
-            DialogResult dialogResult = MessageBox.Show("У ВАС СУПЕР БРАУЗЕР ДЛЯ ГЕЙМЕРОВ?", "", MessageBoxButtons.YesNo);
+            var dialogResult = MessageBox.Show("У ВАС СУПЕР БРАУЗЕР ДЛЯ ГЕЙМЕРОВ?", "", MessageBoxButtons.YesNo);
             if (dialogResult == DialogResult.Yes)
-            {
                 OperaGXCheck();
-            }
-            else if (dialogResult == DialogResult.No)
-            {
-                ChromeCheck();
-            }
-
-
-    
-
+            else if (dialogResult == DialogResult.No) ChromeCheck();
         }
 
         public static void OperaGXCheck()
         {
             var cursongname = "Песни не играет";
-            Process[] procsChrome = Process.GetProcessesByName("opera");
-            if (procsChrome.Length <= 0)
-            {
-                cursongname = "Браузер не открыт";
-            }
+            var procsChrome = Process.GetProcessesByName("opera");
+            if (procsChrome.Length <= 0) cursongname = "Браузер не открыт";
 
 
-            foreach (Process proc in procsChrome)
+            foreach (var proc in procsChrome)
             {
-                if (proc.MainWindowHandle == IntPtr.Zero)
-                {
-                    continue;
-                }
-                AutomationElement root = AutomationElement.FromHandle(proc.MainWindowHandle);
+                if (proc.MainWindowHandle == IntPtr.Zero) continue;
+                var root = AutomationElement.FromHandle(proc.MainWindowHandle);
                 Condition condNewTab = new PropertyCondition(AutomationElement.NameProperty, "Новая вкладка");
-                AutomationElement elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
-                TreeWalker treewalker = TreeWalker.ControlViewWalker;
-                AutomationElement elmTabStrip = treewalker.GetParent(elmNewTab);
-                Condition condTabItem = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
+                var elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
+                var treewalker = TreeWalker.ControlViewWalker;
+                var elmTabStrip = treewalker.GetParent(elmNewTab);
+                Condition condTabItem =
+                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
                 foreach (AutomationElement tabitem in elmTabStrip.FindAll(TreeScope.Children, condTabItem))
-                {
                     if (tabitem.Current.Name.Contains("- YouTube"))
-                    {
                         cursongname = tabitem.Current.Name.Replace("- YouTube", "");
-                    }
-
-                }
             }
+
             Console.WriteLine(cursongname);
-            FileStream fileStream = File.Open("Song.txt", FileMode.Open);
+            var fileStream = File.Open("Song.txt", FileMode.Open);
 
             fileStream.SetLength(0);
             fileStream.Close(); // 
-            using (StreamWriter w = File.AppendText("Song.txt"))
+            using (var w = File.AppendText("Song.txt"))
             {
                 w.Write(cursongname);
                 w.Close();
             }
+
             Thread.Sleep(5000);
             OperaGXCheck();
         }
@@ -126,7 +102,7 @@ namespace MilashkaChannel
         public static void ChromeCheck()
         {
             var cursongname = "Песни не играет";
-            Process[] procsChrome = Process.GetProcessesByName("chrome");
+            var procsChrome = Process.GetProcessesByName("chrome");
             if (procsChrome.Length <= 0)
             {
                 Console.WriteLine("Chrome is not running");
@@ -134,40 +110,34 @@ namespace MilashkaChannel
             }
 
 
-            foreach (Process proc in procsChrome)
+            foreach (var proc in procsChrome)
             {
-                if (proc.MainWindowHandle == IntPtr.Zero)
-                {
-                    continue;
-                }
-                AutomationElement root = AutomationElement.FromHandle(proc.MainWindowHandle);
+                if (proc.MainWindowHandle == IntPtr.Zero) continue;
+                var root = AutomationElement.FromHandle(proc.MainWindowHandle);
                 Condition condNewTab = new PropertyCondition(AutomationElement.NameProperty, "Новая вкладка");
-                AutomationElement elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
-                TreeWalker treewalker = TreeWalker.ControlViewWalker;
-                AutomationElement elmTabStrip = treewalker.GetParent(elmNewTab);
-                Condition condTabItem = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
+                var elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
+                var treewalker = TreeWalker.ControlViewWalker;
+                var elmTabStrip = treewalker.GetParent(elmNewTab);
+                Condition condTabItem =
+                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
                 foreach (AutomationElement tabitem in elmTabStrip.FindAll(TreeScope.Children, condTabItem))
-                {
                     if (tabitem.Current.Name.Contains("YouTube: воспроизводится аудио"))
-                    {
                         cursongname = tabitem.Current.Name.Replace("- YouTube: воспроизводится аудио", "");
-                    }
-
-                }
             }
+
             Console.WriteLine(cursongname);
-            FileStream fileStream = File.Open("Song.txt", FileMode.Open);
+            var fileStream = File.Open("Song.txt", FileMode.Open);
 
             fileStream.SetLength(0);
             fileStream.Close(); // 
-            using (StreamWriter w = File.AppendText("Song.txt"))
+            using (var w = File.AppendText("Song.txt"))
             {
                 w.Write(cursongname);
                 w.Close();
             }
+
             Thread.Sleep(5000);
             ChromeCheck();
         }
-
     }
 }
